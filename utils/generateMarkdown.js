55e9880cb81aadd5e17a0dd1435498968f9f4670
Gentry Hunt <ghunt@csu.edu>
2020-06-17 22:53:52 -0500

    links added

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -70,14 +70,14 @@ ${badgeUrl}
 # Title ${answers.second}
 # Description ${answers.third}
 # Table of Contents 
-[Title]
-[Desciption]
-[Usage]
-[Installation]
-[License]
-[Contributing]
-[Tests]
-[Questions]
+[Title](https://github.com/gentryhunt23/Unit-10-HW/tree/master/utils#title-generatemarkdown)
+[Desciption](https://github.com/gentryhunt23/Unit-10-HW/tree/master/utils#description-this-project-uses-the-cli-to-generate-a-readmemd-file-to-desribe-the-contents-within-the-files-and-the-functionality-of-a-given-proect)
+[Usage](https://github.com/gentryhunt23/Unit-10-HW/tree/master/utils#usage-1)
+[Installation](https://github.com/gentryhunt23/Unit-10-HW/tree/master/utils#installation-2)
+[License](https://github.com/gentryhunt23/Unit-10-HW/tree/master/utils#license-3)
+[Contributing](https://github.com/gentryhunt23/Unit-10-HW/tree/master/utils#contributing-4)
+[Tests](https://github.com/gentryhunt23/Unit-10-HW/tree/master/utils#tests-5)
+[Questions](https://github.com/gentryhunt23/Unit-10-HW/tree/master/utils#questions-6)
 # Usage ${answers.fourth}
 # Installation ${answers.fifth}
 # License ${answers.sixth}
